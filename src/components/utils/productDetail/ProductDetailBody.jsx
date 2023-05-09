66319bae6b2c62030053b43ff0b8de41faa6119c
Soukthavilay Bouphaphan <Soukthavilay2000@gmail.com>
2023-05-10 00:02:33 +0700

    Update product detail body

--- src/components/utils/productDetail/ProductDetailBody.jsx
+++ src/components/utils/productDetail/ProductDetailBody.jsx
@@ -1,13 +1,49 @@
 import React from 'react'
+import ProductFeaturesTable from './ProductFeaturesTable';
 
 function ProductDetailBody() {
+  const productName = "Apple iPhone 14 Pro 512GB 6.1";
   return (
     <>
       <div className="detail-body">
-
+        <div className="detail-body-left">
+          <h3 className="product-detail-title">
+            Product description {productName}
+          </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            omnis voluptas, assumenda accusamus dolorum provident officia qui?
+            Deserunt libero tenetur dolor provident, earum reprehenderit officia
+            numquam minima vitae eligendi unde.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+            recusandae soluta temporibus expedita neque tempora, facere qui
+            voluptatibus praesentium, laboriosam atque commodi, tenetur
+            consequatur enim beatae repellendus corrupti molestiae dolorum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+            recusandae soluta temporibus expedita neque tempora, facere qui
+            voluptatibus praesentium, laboriosam atque commodi, tenetur
+            consequatur enim beatae repellendus corrupti molestiae dolorum.
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
+              recusandae soluta temporibus expedita neque tempora, facere qui
+              voluptatibus praesentium, laboriosam atque commodi, tenetur
+              consequatur enim beatae repellendus corrupti molestiae dolorum.
+            </p>
+          </p>
+        </div>
+        <div className="detail-body-right">
+          <h2 className="product-features-title">
+            <b>Features</b> {productName}
+          </h2>
+          <ProductFeaturesTable />
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default ProductDetailBody
---
+++ src/components/utils/productDetail/ProductFeaturesTable.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function ProductFeaturesTable() {
+  return (
+    <>
+      <div className="product-features">
+        <div className="product-features-item">
+          <span className="features-item-title">Color</span>
+          <span className="feature-item-subtitle">Purple</span>
+        </div>
+        <div className="product-features-item">
+          <span className="features-item-title">Color</span>
+          <span className="feature-item-subtitle">Purple</span>
+        </div>
+        <div className="product-features-item">
+          <span className="features-item-title">Color</span>
+          <span className="feature-item-subtitle">Purple</span>
+        </div>
+        <div className="product-features-item">
+          <span className="features-item-title">Color</span>
+          <span className="feature-item-subtitle">Purple</span>
+        </div>
+        <div className="product-features-item">
+          <span className="features-item-title">Color</span>
+          <span className="feature-item-subtitle">Purple</span>
+        </div>
+        <div className="product-features-item">
+          <span className="features-item-title">Color</span>
+          <span className="feature-item-subtitle">Purple</span>
+        </div>
+        <div className="product-features-item">
+          <span className="features-item-title">Color</span>
+          <span className="feature-item-subtitle">Purple</span>
+        </div>
+        <div className="product-features-item">
+          <span className="features-item-title">Color</span>
+          <span className="feature-item-subtitle">Purple</span>
+        </div>
+        <div className="product-features-item">
+          <span className="features-item-title">Color</span>
+          <span className="feature-item-subtitle">Purple</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductFeaturesTable
