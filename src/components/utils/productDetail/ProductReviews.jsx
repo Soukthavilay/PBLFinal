a643bd3981dad6959799a9e37aa6ec2d4d5e858b
Bouphaphan Soukthavilay <58757620+Soukthavilay@users.noreply.github.com>
2023-06-13 00:02:31 +0700

    review page

--- src/components/utils/productDetail/ProductReviews.jsx
+++ src/components/utils/productDetail/ProductReviews.jsx
@@ -1,56 +1,80 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react';
-import StarRatings from "react-star-ratings";
+import React, { useEffect, useState } from 'react';
+import StarRatings from 'react-star-ratings';
 import ProductReviewDetail from './ProductReviewDetail';
 import axios from 'axios';
 
 function ProductReviews(detailProduct) {
   const dtProduct = detailProduct.detailProduct;
   const idProduct = dtProduct._id;
-  const [result,setResult] = useState(0);
-  const [feedback,setFeedback] = useState([]);
+  const [result, setResult] = useState(0);
+  const [feedback, setFeedback] = useState([]);
+  const [showHideComment, setShowHideComment] = useState(false);
+  const [selectStar, setSelectStar] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(2);
 
-  useEffect(()=>{
-    if(idProduct){
-      const getFeedback = async ()=>{
+  useEffect(() => {
+    if (idProduct) {
+      const getFeedback = async () => {
         try {
           const res = await axios.get(`http://localhost:5000/api/products/${idProduct}`);
-          setFeedback(res.data.feedback)
+          setFeedback(res.data.feedback);
         } catch (error) {
           alert(error.response.data.msg);
         }
-      }
+      };
       getFeedback();
     }
-  },[idProduct]);
-  const [showHideComment, setShowHideComment] = useState(false);
+  }, [idProduct]);
+
   const handleToggle = () => {
     setShowHideComment(!showHideComment);
   };
 
-  const [selectStar, setSelectStar] = useState("");
   const changeRating = (newRating) => {
     setSelectStar({
       rating: newRating,
     });
   };
-  useEffect(()=>{
-    
-    if (feedback && feedback.length){
+
+  useEffect(() => {
+    if (feedback && feedback.length) {
       var total = 0;
-      feedback?.map((FeedbackItem)=>{
+      feedback?.map((FeedbackItem) => {
         total += FeedbackItem.rating;
-      })
+      });
       setResult(total / feedback.length);
     }
-  },[feedback]);
+  }, [feedback]);
+
   const feedbackTotal = feedback?.length || 0;
+
+  const handleLoadMore = () => {
+    setItemsPerPage(itemsPerPage + 8);
+  };
+
+  // Tính toán chỉ mục của phản hồi trong trang hiện tại
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = feedback.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Tạo mảng các số trang
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(feedback.length / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  // Chuyển đổi trang
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <>
       <div className="product-reviews">
-        <h3 className="product-reviews-title">Product Reviews</h3>
+        <h3 className="product-reviews-title">Đánh giá sản phẩm</h3>
         <div className="product-reviews-summary">
-          <div className="reviews-summary-item">
+        <div className="reviews-summary-item">
             <h4>Average rating</h4>
             <span className="average-star">{result} / {feedbackTotal}</span>
             <StarRatings
@@ -75,47 +99,36 @@ function ProductReviews(detailProduct) {
 
         <div
           className={
-            showHideComment ? "comment-container show" : "comment-container hide"
+            showHideComment ? 'comment-container show' : 'comment-container hide'
           }
         >
-          <h3>Write a product review</h3>
-          <div className="comment-form">
-            <input type="text" placeholder="Name" />
-            <StarRatings
-              name="rating"
-              rating={selectStar.rating}
-              changeRating={changeRating}
-              starHoverColor="#fadb14"
-              starRatedColor="#fadb14"
-              starDimension="16px"
-              starSpacing="2px"
-            />
-            <textarea
-              name=""
-              id=""
-              cols="30"
-              rows="5"
-              placeholder="Write comment about product..."
-            ></textarea>
-            <div className="uploadImg">
-              <input
-                type="file"
-                name="file"
-                id="file_up"
-              />
-              <div id="" className="no-line"></div>
-              <div id="file_img">
-                <img src={""} alt="" />
-                <span>X</span>
-              </div>
-            </div>
-            <button className="btn btn--animated btn--primary--blue btn--border--blue">
-              Submit a review
-            </button>
-          </div>
+          {/* Mã code biểu mẫu đánh giá */}
         </div>
+
         {feedback?.length ? (
-          feedback.map((item) => <ProductReviewDetail key={item._id} fd={item} />)
+          <>
+            {currentItems.map((item) => (
+              <ProductReviewDetail key={item._id} fd={item} />
+            ))}
+            {feedback.length > itemsPerPage && (
+              <div className="pagination">
+                {pageNumbers.map((pageNumber) => (
+                  <button
+                    key={pageNumber}
+                    className={pageNumber === currentPage ? 'active' : ''}
+                    onClick={() => handlePageChange(pageNumber)}
+                  >
+                    {pageNumber}
+                  </button>
+                ))}
+              </div>
+            )}
+            {/* {itemsPerPage < feedback.length && (
+              <button className="btn btn--load-more" onClick={handleLoadMore}>
+                Xem thêm
+              </button>
+            )} */}
+          </>
         ) : (
           <p>No reviews available.</p>
         )}
@@ -124,4 +137,4 @@ function ProductReviews(detailProduct) {
   );
 }
 
-export default ProductReviews
+export default ProductReviews;
