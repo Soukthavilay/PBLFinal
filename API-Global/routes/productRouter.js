3306dc3b38db899432d102da1388b6a6e8062f30
Bouphaphan Soukthavilay <58757620+Soukthavilay@users.noreply.github.com>
2023-06-15 15:11:24 +0700

    product router

--- API-Global/routes/productRouter.js
+++ API-Global/routes/productRouter.js
@@ -16,7 +16,7 @@ router.route('/products/:id')
     .delete(auth, authAdmin, productCtrl.deleteProduct)
     .put(auth, authAdmin, productCtrl.updateProduct)
     .get(productCtrl.getDetailFeedback)
-
+router.route('/productItem').get(productCtrl.getProductByCategoryBandTitle);
 router.route('/products/category/:id')
     .get(productCtrl.getProductsByCategory)
 router.route('/products/band/:id')
