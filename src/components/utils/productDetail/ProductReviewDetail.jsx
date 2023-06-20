b26d70b2bdeb3b4ebea2c93d28a8312157a98e03
Soukthavilay Bouphaphan <Soukthavilay2000@gmail.com>
2023-06-20 21:55:25 +0700

    Update reply review

--- src/components/utils/productDetail/ProductReviewDetail.jsx
+++ src/components/utils/productDetail/ProductReviewDetail.jsx
@@ -1,4 +1,5 @@
 import StarRatings from "react-star-ratings";
+import { useState } from 'react';
 
 function ProductReviewDetail(fd) {
   const feedback = fd.fd;
@@ -8,27 +9,74 @@ function ProductReviewDetail(fd) {
     (createdAtDateTime - currentDateTime) / (1000 * 60)
   );
   const formattedDateTime = createdAtDateTime.toLocaleString();
+  const [showHideComment, setShowHideComment] = useState(false);
+  const handleToggle = () => {
+    setShowHideComment(!showHideComment);
+  };
   return (
     <>
       <div className="product-reviews-detail">
         <div className="reviews-detail-item">
           <div className="reviews-detail-avatar">
-            <span>{feedback.username}</span>
+            <span>{feedback.username?.charAt(0)}</span>
           </div>
           <div className="reviews-detail-content">
-            <h5 className="reviews-username">{feedback.username}</h5>
-            <StarRatings
-              name="rating"
-              rating={feedback.rating}
-              starRatedColor="#fadb14"
-              starDimension="16px"
-              starSpacing="2px"
-            />
-            <p>{formattedDateTime} </p>
-            <p className="reviews-comment-detail">
-              {minutesDifference} {feedback.content} 
-            </p>
+            <div className="review-detail-content_header">
+              <div className="review-detail-content_header-left">
+                <h5 className="reviews-username">{feedback.username}</h5>
+                <StarRatings
+                  name="rating"
+                  rating={feedback.rating}
+                  starRatedColor="#fadb14"
+                  starDimension="16px"
+                  starSpacing="2px"
+                />
+              </div>
+              <div className="review-detail-content_header-right">
+                <p>{formattedDateTime} </p>
+                {/* <p className="reviews-comment-detail">
+                  {minutesDifference}
+                </p> */}
+              </div>
+            </div>
             <img src={feedback.image_url} alt={feedback.image_url} width={200} />
+            <p className="review-comment-text">{feedback.content}</p>
+            
+            {/* REPLY SECTION */}
+            <div className="review-reply">
+              <button className="reply-btn" onClick={handleToggle}>Reply</button>
+              <div className={showHideComment ? 'review-reply-form show' : 'review-reply-form hide'}>
+                <textarea name="" id="" cols="30" rows="10" placeholder="Write reply..."></textarea>
+                <div className="reply-submit-btn">
+                  <button className="btn btn--animated btn--primary--blue btn--border--blue">Send reply</button>
+                </div>
+              </div>
+            </div>
+            
+            {/* REPLY CONTENT */}
+            <div className="reply-content">
+              <div className="reply-content-item">
+                <div className="reviews-detail-avatar">
+                  <span>K</span>
+                </div>
+                <div className="reply-detail">
+                  <div className="reply-content-item_header">
+                    <div className="reply-content-item_header-left">
+                      <div className="reply-content-username">
+                        <span>Koh Koh</span>
+                        <span className="reply-content-username_role">Admin</span>
+                      </div>
+                    </div>
+                    <div className="reply-content-item_header-right">
+                      <p>{formattedDateTime} </p>
+                    </div>
+                  </div>
+                  <div className="reply-content-item_detail">
+                    <p>San pham verigut</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
