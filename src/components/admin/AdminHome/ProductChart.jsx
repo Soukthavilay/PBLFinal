ce58e96f8003ee97e96882bd0aa3f64fe8b30883
Bouphaphan Soukthavilay <58757620+Soukthavilay@users.noreply.github.com>
2023-06-28 01:30:53 +0700

    update dashboard data

--- src/components/admin/AdminHome/ProductChart.jsx
+++ src/components/admin/AdminHome/ProductChart.jsx
@@ -1,78 +1,48 @@
-import React, { PureComponent } from 'react'
+import React, { useEffect, useState } from 'react';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 function ProductChart() {
+  const [statistics, setStatistics] = useState(null);
 
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+  useEffect(() => {
+    fetch('http://localhost:5000/api/statistics/allMonth')
+      .then(response => response.json())
+      .then(data => setStatistics(data))
+      .catch(error => console.log(error));
+  }, []);
 
+  if (statistics === null || statistics === undefined) {
+    return <p>Loading...</p>;
+  }
+
+  const monthData = Array.from({ length: 12 }, (_, index) => {
+    const month = index + 1;
+    const name = `${month.toString().padStart(2, '0')}`;
+    const value = statistics.find(item => item.month === month)?.totalOrders || 0;
+  
+    return { name, value };
+  });
   return (
-    <>
-
-
-      <ResponsiveContainer width="100%" height={400}>
-        <AreaChart
-          width={500}
-          height={400}
-          data={data}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-        </AreaChart>
-      </ResponsiveContainer>
-    </>
-  )
+    <ResponsiveContainer width="100%" height={400}>
+      <AreaChart
+        width={500}
+        height={400}
+        data={monthData}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="value" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 }
 
-export default ProductChart
+export default ProductChart;
